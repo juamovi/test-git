b199bb8deb31fbcae6c8004968eec850cad280d8
JuanAn Morant <juamovi@hotmail.com>
2020-03-28 17:52:57 +0100

    Afegint exercici Infinito

---
+++ infinito/src/infinito/Infinito.java
@@ -0,0 +1,15 @@
+package infinito;
+
+public class Infinito {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num, res=0; 
+		num = 1;
+		while(num==1) {
+			res = num + res;
+			System.out.println(" Sumando : " + res);
+		}
+	}
+
+}
